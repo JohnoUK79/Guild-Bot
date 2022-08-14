4a7ac464a5b978901ccc83d17966a9f8a3fc701e
John O'Brien <JohnoUK79@gmail.com>
2022-08-14 16:55:20 +0000

    Update ADd & Remove Embeds

--- Events/guildMemberRemove.js
+++ Events/guildMemberRemove.js
@@ -8,17 +8,17 @@ module.exports = {
         Data = await sql.Execute(`select * from settings where guild_id = '${member.guild.id}';`); 
         GUILD = member.guild.name
         Channel_ID = Data[0].welcome_channel_id
-            const MemberEmbed = new Discord.MessageEmbed()
+            const goodByeEmbed = new Discord.MessageEmbed()
             .setColor("#d81e5b")
             .setTitle("Player Left!")
-            .setDescription(`${member.user} has left the server! \nThey could not handle the TRUTH!.`)
+            .setDescription(`<@${member.user.id}> has left the server! \nThey could not handle the TRUTH!.`)
             .setThumbnail(member.user.displayAvatarURL())
             .setFooter({ text: `${GUILD}`, iconURL: 'https://i.imgur.com/IxBdjfl.jpg' })
             .setTimestamp();
 
             member.guild.channels.cache.get(Channel_ID).send(
                 {
-                    embeds: [MemberEmbed]
+                    embeds: [goodByeEmbed]
                 }
             )
     }

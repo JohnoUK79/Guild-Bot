131f57f3a379409dd684a8857247530f1a25facf
John O'Brien <JohnoUK79@gmail.com>
2022-08-21 15:34:38 +0000

    More Time Updates

--- Events/presenceUpdate.js
+++ Events/presenceUpdate.js
@@ -1,11 +1,12 @@
-const timestamp = require('time-stamp');
+const time = require('../config/timestamp');
+setDate = time.UTCdefault()
 module.exports = {
     name: 'presenceUpdate',
     async execute(oldPresence, newPresence) {
         if (oldPresence)
-            console.log('Presence Change:', (timestamp.utc('YYYY/MM/DD HH:mm:ss')), 'Server:', newPresence.guild.name, 'User:', newPresence.member.displayName, 'From:', oldPresence.status ?? 'No Prior', 'To:', newPresence.status);
+            console.log('Presence Change:', (setDate), 'Server:', newPresence.guild.name, 'User:', newPresence.member.displayName, 'From:', oldPresence.status ?? 'No Prior', 'To:', newPresence.status);
             
         else
-            console.log('Presence Change:', (timestamp.utc('YYYY/MM/DD HH:mm:ss')), 'Server:', newPresence.guild.name, 'User:', newPresence.member.displayName, 'From: No Prior', 'To:', newPresence.status);
+            console.log('Presence Change:', (setDate), 'Server:', newPresence.guild.name, 'User:', newPresence.member.displayName, 'From: No Prior', 'To:', newPresence.status);
     },
 };
--- Events/ready.js
+++ Events/ready.js
@@ -1,8 +1,9 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const { token, CLIENT_ID, GUILD_ID } = require('../config.json');
-const timestamp = require('time-stamp');
 const nodeCron = require("node-cron");
+const time = require('../config/timestamp');
+setDate = time.UTCdefault()
 
 module.exports = {
     name: 'ready',
@@ -12,7 +13,7 @@ module.exports = {
         console.log(client.guilds.cache.map(r => `${r.id} - ${r.name} - ${r.icon} - ${r.ownerId} - ${r.description} - ${r.preferredLocale} - ${r.publicUpdatesChannelId} - ${r.systemChannelId} - ${r.rulesChannelId}`));
         const guild = client.guilds.cache
         //console.log(guild)
-        console.log(`${(timestamp.utc('YYYY/MM/DD HH:mm:ss'))} - Logged in as - ${client.user.tag}`);
+        console.log(`${setDate} - Logged in as - ${client.user.tag}`);
         const rest = new REST({ version: '10' }).setToken(token);
         await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
             .then(() => {

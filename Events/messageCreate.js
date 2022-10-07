022e01ff1a424580f8164ae9e0ab165a43138728
John O'Brien <JohnoUK79@gmail.com>
2022-10-07 20:28:28 +0000

    Added Levels Rank Roles

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -10,10 +10,9 @@ module.exports = {
 		if (message.author.bot === true) {
 			return;
 		}
-		
 		Settings = await sql.Execute(`select * from settings where guild_id = '${message.guild.id}';`); 
 		Levels = await sql.Execute(`select * from levels where discord_id = '${message.author.id}';`); 
-
+		var score = Math.floor(Math.random() * 150) * 3;
 		GuildName = message.guild.name
 
 		const updatePlayer =  new MessageActionRow()
@@ -23,6 +22,19 @@ module.exports = {
 				.setLabel('Add / Update your Search Profile!')
 				.setStyle('PRIMARY'),
 		)
+
+		const devSupport = new MessageEmbed()
+		.setColor('#0099ff')
+		.setTitle('Buy the Dev A Beer!')
+		.setURL('https://www.buymeacoffee.com/johnouk79')
+		.setThumbnail(message.member.displayAvatarURL())
+		.setAuthor({ name: message.member.displayName, iconURL: message.member.displayAvatarURL({ dynamic: true }), url: '' })
+		.setDescription(`**Enjoying the Bot? Buy the Dev a Beer**!`)
+		.addFields(
+			{ name: `Buy Now!:`, value: `https://www.buymeacoffee.com/johnouk79` },
+			)
+		.setFooter({ text: 'Buy Dekes A Beer!.', iconURL: 'http://phfamily.co.uk/img/gifs/PH-Family-Dark.jpg' });
+
 
 		const newPlayer = new MessageEmbed()
 		.setColor('#0099ff')
@@ -40,8 +52,6 @@ module.exports = {
 		.setTimestamp()
 		.setFooter({ text: 'Welcome to the PH Family.', iconURL: 'http://phfamily.co.uk/img/gifs/PH-Family-Dark.jpg' });
 
-		var score = Math.floor(Math.random() * 150) * 3;
-
 		if (Levels.length === 0) {
 			console.log("New Member")
 			playerImage = "http://phfamily.co.uk/img/gifs/NotFound.png"
@@ -53,9 +63,7 @@ module.exports = {
 				components: [updatePlayer],
 				embeds: [newPlayer]
 			});
-			return;
-
-			
+			return;			
 		}
 
 		Lookup = Levels[0].player_id
@@ -69,69 +77,63 @@ module.exports = {
 			playerId = Levels[0].player_id
 			updatePlayers = await sql.Execute(`UPDATE players SET date_last_known = '${setDate}', discord ='${message.author.id}', discord_server = '${GuildName}' WHERE player_id = ${playerId}`)}
 
-
-		//let roleRank10 = Settings[0].Rank_10
-		//let roleRank20 = Settings[0].Rank_20
-		//let roleRank30 = Settings[0].Rank_30
-		//let roleRank40 = Settings[0].Rank_40
-		//let roleRank50 = Settings[0].Rank_50
-		//let roleRank60 = Settings[0].Rank_60
-		//let roleRank70 = Settings[0].Rank_70
-		//let roleRank80 = Settings[0].Rank_80
-		//let roleRank90 = Settings[0].Rank_90
-		//let roleRank100 = Settings[0].Rank_100
-		let scoreLevel = Levels[0].level
-		//let r10name = message.guild.roles.cache.find( r => r.id === rank10 )
+		let roleRank10 = Settings[0].Rank_10
+		let roleRank20 = Settings[0].Rank_20
+		let roleRank30 = Settings[0].Rank_30
+		let roleRank40 = Settings[0].Rank_40
+		let roleRank50 = Settings[0].Rank_50
+		let roleRank60 = Settings[0].Rank_60
+		let roleRank70 = Settings[0].Rank_70
+		let roleRank80 = Settings[0].Rank_80
+		let roleRank90 = Settings[0].Rank_90
+		let roleRank100 = Settings[0].Rank_100
 		
+		let scoreLevel = Levels[0].level
+		let r10name = message.guild.roles.cache.find( r => r.id === roleRank10 )
+		let r20name = message.guild.roles.cache.find( r => r.id === roleRank20 )
+		let r30name = message.guild.roles.cache.find( r => r.id === roleRank30 )
+		let r40name = message.guild.roles.cache.find( r => r.id === roleRank40 )
+		let r50name = message.guild.roles.cache.find( r => r.id === roleRank50 )
+		let r60name = message.guild.roles.cache.find( r => r.id === roleRank60 )
+		let r70name = message.guild.roles.cache.find( r => r.id === roleRank70 )
+		let r80name = message.guild.roles.cache.find( r => r.id === roleRank80 )
+		let r90name = message.guild.roles.cache.find( r => r.id === roleRank90 )
+		let r100name = message.guild.roles.cache.find( r => r.id === roleRank100 )
+
 		if (scoreLevel > 20) {
 			var score = Math.floor(Math.random() * 150) * 2;
 		}
-
 		if (scoreLevel > 30) {
 			var score = Math.floor(Math.random() * 125) * 2;
-
 		}
-
 		if (scoreLevel > 40) {
 			var score = Math.floor(Math.random() * 100) * 2;
-
 		}	
-
 		if (scoreLevel > 50) {
 			var score = Math.floor(Math.random() * 75) * 2;
-
 		}	
-
 		if (scoreLevel > 60) {
 			var score = Math.floor(Math.random() * 50) * 2;
-
 		}	
-
 		if (scoreLevel > 70) {
 			var score = Math.floor(Math.random() * 25) + 1;
-
 		} 
-
 		if (scoreLevel > 80) {
 			var score = Math.floor(Math.random() * 15) +1;
-
 		} 
-
 		if (scoreLevel > 90) {
 			var score = Math.floor(Math.random() * 10) +1;
-
 		} 
-
 		if (scoreLevel > 100) {
 			var score = Math.floor(Math.random() * 5) +1;
-
 		} 
 			
 		points = Levels[0].points
 		newPoints = (points + score)
 		newLevel = (Levels[0].level + 1)
 		let LevelUpChannel = Settings[0].level_up_channel_id
-		
+		console.log(LevelUpChannel)
+
 		const levelup = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Level Up')
@@ -151,27 +153,173 @@ module.exports = {
 
 		let initiallevel = Levels[0].level
 		level = Math.floor((score + points) / 3666 )
-/*
-		  if (level === 10) {
-			if (!rank10) {
-				console.log("No Role Set")
-				let result = await sql.Execute (`UPDATE levels SET points = '${newPoints}', level = '${level}', discord_username = '${message.member.displayName}', last_seen_server = '${GuildName}' WHERE discord_id = '${message.author.id}'`)
-				await message.reply({ content: 'Congratulations you have Ranked Up and achieved the rank of **PRIVATE**'})
-				return;
-				
-			} else await message.member.roles.add(rank10).catch((e) => console.log(e))
-				let result = await sql.Execute (`UPDATE levels SET points = '${newPoints}', level = '${level}', discord_username = '${message.member.displayName}', last_seen_server = '${GuildName}' WHERE discord_id = '${message.author.id}'`)
-				await message.reply({ content: `Congratulations you have Ranked Up and achieved the rank of ${r10name}` })
-				return:
-		}  
- 
-*/
+
 		if (level > initiallevel) {
 			console.log("Level Up")
  			await message.guild.channels.cache.get(LevelUpChannel).send({
 				embeds: [levelup],
 				components: [updatePlayer],
 			}) 
+
+			if (level === 10) {
+				console.log("Rank Up")
+				if (!roleRank10) {
+					console.log("No Role Set")
+					await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **Private**`})
+					await message.guild.channels.cache.get(LevelUpChannel).send({
+						embeds: [devSupport],
+					}) 					
+				} else {
+				await message.member.roles.add(roleRank10).catch((e) => console.log(e))
+				await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r10name.name}**` })
+				await message.guild.channels.cache.get(LevelUpChannel).send({
+					embeds: [devSupport],
+				})}
+				} 
+
+				if (level === 20) {
+					console.log("Rank Up")
+					if (!roleRank20) {
+						console.log("No Role Set")
+						await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **Corporal**`})
+						await message.guild.channels.cache.get(LevelUpChannel).send({
+							embeds: [devSupport],
+						}) 						
+					} else {
+					await message.member.roles.add(roleRank20).catch((e) => console.log(e))
+					await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r20name.name}**` })
+					await message.guild.channels.cache.get(LevelUpChannel).send({
+						embeds: [devSupport],
+					})} 
+					} 
+
+					if (level === 30) {
+						console.log("Rank Up")
+						if (!roleRank30) {
+							console.log("No Role Set")
+							await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **Sergeant**`})
+							await message.guild.channels.cache.get(LevelUpChannel).send({
+								embeds: [devSupport],
+							}) 
+						} else {
+						await message.member.roles.add(roleRank30).catch((e) => console.log(e))
+						await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r30name.name}**` })
+						await message.guild.channels.cache.get(LevelUpChannel).send({
+							embeds: [devSupport],
+						})} 
+						} 
+
+						if (level === 40) {
+							console.log("Rank Up")
+							if (!roleRank40) {
+								console.log("No Role Set")
+								await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **Lieutenant**`})
+								await message.guild.channels.cache.get(LevelUpChannel).send({
+									embeds: [devSupport],
+								}) 
+							} else {
+							await message.member.roles.add(roleRank40).catch((e) => console.log(e))
+							await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r40name.name}**` })
+							await message.guild.channels.cache.get(LevelUpChannel).send({
+								embeds: [devSupport],
+							})} 
+							} 
+
+							if (level === 50) {
+								console.log("Rank Up")
+								if (!roleRank50) {
+									console.log("No Role Set")
+									await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **Captain**`})
+									await message.guild.channels.cache.get(LevelUpChannel).send({
+										embeds: [devSupport],
+									}) 
+								} else {
+								await message.member.roles.add(roleRank50).catch((e) => console.log(e))
+								await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r50name.name}**` })
+								await message.guild.channels.cache.get(LevelUpChannel).send({
+									embeds: [devSupport],
+								})} 
+								} 
+
+								if (level === 60) {
+									console.log("Rank Up")
+									if (!roleRank60) {
+										console.log("No Role Set")
+										await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **Major**`})
+										await message.guild.channels.cache.get(LevelUpChannel).send({
+											embeds: [devSupport],
+										}) 
+									} else {
+									await message.member.roles.add(roleRank60).catch((e) => console.log(e))
+									await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r60name.name}**` })
+									await message.guild.channels.cache.get(LevelUpChannel).send({
+										embeds: [devSupport],
+									})} 
+									} 
+
+									if (level === 70) {
+										console.log("Rank Up")
+										if (!roleRank70) {
+											console.log("No Role Set")
+											await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **Colonel**`})
+											await message.guild.channels.cache.get(LevelUpChannel).send({
+												embeds: [devSupport],
+											}) 
+										} else {
+										await message.member.roles.add(roleRank70).catch((e) => console.log(e))
+										await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r70name.name}**` })
+										await message.guild.channels.cache.get(LevelUpChannel).send({
+											embeds: [devSupport],
+										})} 
+										} 
+
+										if (level === 80) {
+											console.log("Rank Up")
+											if (!roleRank80) {
+												console.log("No Role Set")
+												await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **Major General**`})
+												await message.guild.channels.cache.get(LevelUpChannel).send({
+													embeds: [devSupport],
+												}) 
+											} else {
+											await message.member.roles.add(roleRank80).catch((e) => console.log(e))
+											await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r80name.name}**` })
+											await message.guild.channels.cache.get(LevelUpChannel).send({
+												embeds: [devSupport],
+											})} 
+											} 
+
+											if (level === 90) {
+												console.log("Rank Up")
+												if (!roleRank90) {
+													console.log("No Role Set")
+													await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **General**`})
+													await message.guild.channels.cache.get(LevelUpChannel).send({
+														embeds: [devSupport],
+													}) 
+												} else {
+												await message.member.roles.add(roleRank90).catch((e) => console.log(e))
+												await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r90name.name}**` })
+												await message.guild.channels.cache.get(LevelUpChannel).send({
+													embeds: [devSupport],
+												})} 
+												} 
+
+												if (level === 100) {
+													console.log("Rank Up")
+													if (!roleRank100) {
+														console.log("No Role Set")
+														await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **General of The Army**`})
+														await message.guild.channels.cache.get(LevelUpChannel).send({
+															embeds: [devSupport],
+														}) 
+													} else {
+													await message.member.roles.add(roleRank100).catch((e) => console.log(e))
+													await message.guild.channels.cache.get(LevelUpChannel).send({ content: `Congratulations **<@${message.member.id}>**, you have Ranked Up and achieved the rank of **${r100name.name}**` })
+													await message.guild.channels.cache.get(LevelUpChannel).send({
+														embeds: [devSupport],
+													})} 
+													} 
 		}
 		let result = await sql.Execute (`UPDATE levels SET points = '${newPoints}', level = '${level}', discord_username = '${message.member.displayName}', last_seen_server = '${GuildName}' WHERE discord_id = '${message.author.id}'`)}
-}; 
+	}; 
---
+++ commands/set-ranks.js
@@ -0,0 +1,139 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { PermissionFlagsBits } = require('discord-api-types/v10');
+const { MessageEmbed, Client, ModalSubmitFieldsResolver, MessageActionRow, MessageButton, Message } = require('discord.js');
+const sql = require("../config/Database");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator | PermissionFlagsBits.ManageGuild)
+
+        .setName("set-ranks")
+        .setDescription("Setup the Rank Roles for Levels")
+
+        .addStringOption((option) => option
+            .setName("rank10")
+            .setDescription("Set the Role for Rank 10 / Private!")
+            .setRequired(true)
+        )
+        
+        .addStringOption((option) => option
+            .setName("rank20")
+            .setDescription("Set the Role for Rank 20 / Corporal!")
+            .setRequired(false)
+        )
+                
+        .addStringOption((option) => option
+            .setName("rank30")
+            .setDescription("Set the Role for Rank 30 / Sergeant!")
+            .setRequired(false)
+        )
+                        
+        .addStringOption((option) => option
+            .setName("rank40")
+            .setDescription("Set the Role for Rank 40 / Lieutenant!")
+            .setRequired(false)
+        )
+                        
+        .addStringOption((option) => option
+            .setName("rank50")
+            .setDescription("Set the Role for Rank 50 / Captain!")
+            .setRequired(false)
+        )
+                        
+        .addStringOption((option) => option
+            .setName("rank60")
+            .setDescription("Set the Role for Rank 60 / Major!")
+            .setRequired(false)
+        )
+                        
+        .addStringOption((option) => option
+            .setName("rank70")
+            .setDescription("Set the Role for Rank 70 / Colonel!")
+            .setRequired(false)
+        )
+                        
+        .addStringOption((option) => option
+            .setName("rank80")
+            .setDescription("Set the Role for Rank 80 / Major General!")
+            .setRequired(false)
+        )
+                        
+        .addStringOption((option) => option
+            .setName("rank90")
+            .setDescription("Set the Role for Rank 90 / General!")
+            .setRequired(false)
+        )
+                        
+        .addStringOption((option) => option
+            .setName("rank100")
+            .setDescription("Set the Role for Rank 100 / General of The Army!")
+            .setRequired(false)
+        ),
+
+    async execute(Interaction) {
+        
+        var guildId = Interaction.guildId
+        var message = Interaction.options.getString('message');
+        var emoji = Interaction.options.getString('emoji');
+        var role = Interaction.options.getString('role');
+
+        var level10 = Interaction.options.getString('rank10');
+        var level20 = Interaction.options.getString('rank20');
+        var level30 = Interaction.options.getString('rank30');
+        var level40 = Interaction.options.getString('rank40');
+        var level50 = Interaction.options.getString('rank50');
+        var level60 = Interaction.options.getString('rank60');
+        var level70 = Interaction.options.getString('rank70');
+        var level80 = Interaction.options.getString('rank80');
+        var level90 = Interaction.options.getString('rank90');
+        var level100 = Interaction.options.getString('rank100');
+
+
+
+
+        //console.log(Interaction)
+        const addRanks = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`PH Family`)
+            .setURL('http://www.phfamily.co.uk/')
+            .setThumbnail(Interaction.user.displayAvatarURL())
+            .setAuthor({ name: Interaction.member.displayName, iconURL: Interaction.user.displayAvatarURL({ dynamic: true }), url: '' })
+            .setDescription(`**Levels Rank Roles Added!**`)
+            .setThumbnail('http://phfamily.co.uk/img/gifs/Poll.gif')
+            .addFields(
+                { name: `Rank 10`, value: `${level10}` },
+                { name: `Rank 20`, value: `${level20}` },
+                { name: `Rank 30`, value: `${level30}` },
+                { name: `Rank 40`, value: `${level40}` },
+                { name: `Rank 50`, value: `${level50}` },
+                { name: `Rank 60`, value: `${level60}` },
+                { name: `Rank 70`, value: `${level70}` },
+                { name: `Rank 80`, value: `${level80}` },
+                { name: `Rank 90`, value: `${level90}` },
+                { name: `Rank 100`, value: `${level100}` },
+            )
+            .setImage(`http://phfamily.co.uk/img/gifs/PH-Family-Red.jpg`)
+            .setTimestamp()
+            .setFooter({ text: 'PH Family - Rank Roles.', iconURL: 'http://phfamily.co.uk/img/gifs/PH-Family-Red.jpg' });
+            await Interaction.reply({
+
+            ephemeral: true,
+            embeds: [addRanks],
+        });
+        
+        let r10 = level10.replace(/\D+/g, '');
+        let r20 = level20.replace(/\D+/g, '');
+        let r30 = level30.replace(/\D+/g, '');
+        let r40 = level40.replace(/\D+/g, '');
+        let r50 = level50.replace(/\D+/g, '');
+        let r60 = level60.replace(/\D+/g, '');
+        let r70 = level70.replace(/\D+/g, '');
+        let r80 = level80.replace(/\D+/g, '');
+        let r90 = level90.replace(/\D+/g, '');
+        let r100 = level100.replace(/\D+/g, '');
+
+
+        updateRanks = await sql.Execute(`INSERT INTO settings (guild_id, Rank_10, Rank_20, Rank_30, Rank_40, Rank_50, Rank_60, Rank_70, Rank_80, Rank_90, Rank_100) VALUES ('${guildId}', '${r10}', '${r20}', '${r30}', '${r40}', '${r50}', '${r60}', '${r70}', '${r80}', '${r90}', '${r100}') ON DUPLICATE KEY UPDATE Rank_10 = '${r10}', Rank_20 = '${r20}', Rank_30 = '${r30}', Rank_40 = '${r40}', Rank_50 = '${r50}', Rank_60 = '${r60}', Rank_70 = '${r70}', Rank_80 = '${r80}', Rank_90 = '${r90}', Rank_100 = '${r100}';`)
+        console.log(updateRanks)
+    },
+};

bf750822ee26153a652b90bda2cabc6ccfd280b8
John O'Brien <JohnoUK79@gmail.com>
2022-10-09 17:23:30 +0000

    Fixed Add Ranks Order

--- commands/add-ranks.js
+++ commands/add-ranks.js
@@ -11,145 +11,23 @@ module.exports = {
 			empheral: true,
 			fetchReply: true,
 		});
-
-		//Private
-		let rolePrivate = interaction.guild.roles.cache.find(role => role.name === "Private");
-		if (!rolePrivate) {
+		//GeneralArmy
+		let roleGeneralArmy = interaction.guild.roles.cache.find(role => role.name === "General of the Army");
+		if (!roleGeneralArmy) {
 			console.log(`No Role Found`)
-			let rolePrivate = await interaction.guild.roles.create({ 
-				name: 'Private',
-				color: '#2e8f37', //forest green
+			let roleGeneralArmy = await interaction.guild.roles.create({ 
+				name: 'General of the Army',
+				color: '#ffff00', //yellow
 				mentionable: true,
+				hoist: true,
 		})
 		console.log(`New Role Created`)
 		} else {
 		console.log(`Existing Role`)
-		rolePrivate.edit({
-			color: '#2e8f37', //forest green
+		roleGeneralArmy.edit({
+			color: '#ffff00', //yellow
 			mentionable: true,
-		})
-		}
-		//Corporal
-		let roleCorporal = interaction.guild.roles.cache.find(role => role.name === "Corporal");
-		if (!roleCorporal) {
-			console.log(`No Role Found`)
-			let roleCorporal = await interaction.guild.roles.create({ 
-				name: 'Corporal',
-				color: '#00ff00', //green
-				mentionable: true,
-		})
-		console.log(`New Role Created`)
-		} else {
-		console.log(`Existing Role`)
-		roleCorporal.edit({
-			color: '#00ff00', //green
-			mentionable: true,
-		})
-		}
-		//Sergeant
-		let roleSergeant = interaction.guild.roles.cache.find(role => role.name === "Sergeant");
-		if (!roleSergeant) {
-			console.log(`No Role Found`)
-			let roleSergeant = await interaction.guild.roles.create({ 
-				name: 'Sergeant',
-				color: '#00ff80', //spring green
-				mentionable: true,
-		})
-		console.log(`New Role Created`)
-		} else {
-		console.log(`Existing Role`)
-		roleSergeant.edit({
-			color: '#00ff80', //spring green
-			mentionable: true,
-		})
-		}
-
-		//Lieutenant
-		let roleLieutenant = interaction.guild.roles.cache.find(role => role.name === "Lieutenant");
-		if (!roleLieutenant) {
-			console.log(`No Role Found`)
-			let roleLieutenant = await interaction.guild.roles.create({ 
-				name: 'Lieutenant',
-				color: '#00ffff', //cyan
-				mentionable: true,
-		})
-		console.log(`New Role Created`)
-		} else {
-		console.log(`Existing Role`)
-		roleLieutenant.edit({
-			color: '#00ffff', //cyan
-			mentionable: true,
-		})
-		}
-
-		//Captain
-		let roleCaptain = interaction.guild.roles.cache.find(role => role.name === "Captain");
-		if (!roleCaptain) {
-			console.log(`No Role Found`)
-			let roleCaptain = await interaction.guild.roles.create({ 
-				name: 'Captain',
-				color: '#0080ff', //dodger blue
-				mentionable: true,
-		})
-		console.log(`New Role Created`)
-		} else {
-		console.log(`Existing Role`)
-		roleCaptain.edit({
-			color: '#0080ff', //dodger blue
-			mentionable: true,
-		})
-		}
-		
-		//Major
-		let roleMajor = interaction.guild.roles.cache.find(role => role.name === "Major");
-		if (!roleMajor) {
-			console.log(`No Role Found`)
-			let roleMajor = await interaction.guild.roles.create({ 
-				name: 'Major',
-				color: '#0000ff', //blue
-				mentionable: true,
-		})
-		console.log(`New Role Created`)
-		} else {
-		console.log(`Existing Role`)
-		roleMajor.edit({
-			color: '#0000ff', //blue
-			mentionable: true,
-		})
-		}
-		//Colonel
-		let roleColonel = interaction.guild.roles.cache.find(role => role.name === "Colonel");
-		if (!roleColonel) {
-			console.log(`No Role Found`)
-			let roleColonel = await interaction.guild.roles.create({ 
-				name: 'Colonel',
-				color: '#8000ff', //purple
-				mentionable: true,
-		})
-		roleColonel.setPosition(10)
-		console.log(`New Role Created`)
-		} else {
-		console.log(`Existing Role`)
-		roleColonel.edit({
-			color: '#8000ff', //purple
-			mentionable: true,
-		})
-		}
-		//MajorGeneral
-		let roleMajorGeneral = interaction.guild.roles.cache.find(role => role.name === "Major General");
-		if (!roleMajorGeneral) {
-			console.log(`No Role Found`)
-			let roleMajorGeneral = await interaction.guild.roles.create({ 
-				name: 'Major General',
-				color: '#ff0080', //magenta
-				mentionable: true,
-		})
-		console.log(`New Role Created`)
-		} else {
-		console.log(`Existing Role`)
-		roleMajorGeneral.edit({
-			color: '#ff0080', //magenta
-			mentionable: true,
+			hoist: true,
 		})
 		}		
 		//General
@@ -160,6 +38,7 @@ module.exports = {
 				name: 'General',
 				color: '#ff0000', //red
 				mentionable: true,
+				hoist: true,
 		})
 		console.log(`New Role Created`)
 		} else {
@@ -167,25 +46,162 @@ module.exports = {
 		roleGeneral.edit({
 			color: '#ff0000', //red
 			mentionable: true,
+			hoist: true,
 		})
-		}		
-		//GeneralArmy
-		let roleGeneralArmy = interaction.guild.roles.cache.find(role => role.name === "General of the Army");
-		if (!roleGeneralArmy) {
+		}	
+		//MajorGeneral
+		let roleMajorGeneral = interaction.guild.roles.cache.find(role => role.name === "Major General");
+		if (!roleMajorGeneral) {
 			console.log(`No Role Found`)
-			let roleGeneralArmy = await interaction.guild.roles.create({ 
-				name: 'General of the Army',
-				color: '#ffff00', //yellow
+			let roleMajorGeneral = await interaction.guild.roles.create({ 
+				name: 'Major General',
+				color: '#ff0080', //magenta
 				mentionable: true,
+				hoist: true,
 		})
 		console.log(`New Role Created`)
 		} else {
 		console.log(`Existing Role`)
-		roleGeneralArmy.edit({
-			color: '#ffff00', //yellow
+		roleMajorGeneral.edit({
+			color: '#ff0080', //magenta
 			mentionable: true,
+			hoist: true,
 		})
 		}		
-		await interaction.editReply({ empheral: true, content: 'Rank Roles Added!' })
+		//Colonel
+		let roleColonel = interaction.guild.roles.cache.find(role => role.name === "Colonel");
+		if (!roleColonel) {
+			console.log(`No Role Found`)
+			let roleColonel = await interaction.guild.roles.create({ 
+				name: 'Colonel',
+				color: '#8000ff', //purple
+				mentionable: true,
+				hoist: true,
+		})
+		roleColonel.setPosition(10)
+		console.log(`New Role Created`)
+		} else {
+		console.log(`Existing Role`)
+		roleColonel.edit({
+			color: '#8000ff', //purple
+			mentionable: true,
+			hoist: true,
+		})
+		}
+		//Major
+		let roleMajor = interaction.guild.roles.cache.find(role => role.name === "Major");
+		if (!roleMajor) {
+			console.log(`No Role Found`)
+			let roleMajor = await interaction.guild.roles.create({ 
+				name: 'Major',
+				color: '#0000ff', //blue
+				mentionable: true,
+				hoist: true,
+		})
+		console.log(`New Role Created`)
+		} else {
+		console.log(`Existing Role`)
+		roleMajor.edit({
+			color: '#0000ff', //blue
+			mentionable: true,
+			hoist: true,
+		})
+		}
+		//Captain
+		let roleCaptain = interaction.guild.roles.cache.find(role => role.name === "Captain");
+		if (!roleCaptain) {
+			console.log(`No Role Found`)
+			let roleCaptain = await interaction.guild.roles.create({ 
+				name: 'Captain',
+				color: '#0080ff', //dodger blue
+				mentionable: true,
+				hoist: true,
+		})
+		console.log(`New Role Created`)
+		} else {
+		console.log(`Existing Role`)
+		roleCaptain.edit({
+			color: '#0080ff', //dodger blue
+			mentionable: true,
+			hoist: true,
+		})
+		}
+		//Lieutenant
+		let roleLieutenant = interaction.guild.roles.cache.find(role => role.name === "Lieutenant");
+		if (!roleLieutenant) {
+			console.log(`No Role Found`)
+			let roleLieutenant = await interaction.guild.roles.create({ 
+				name: 'Lieutenant',
+				color: '#00ffff', //cyan
+				mentionable: true,
+				hoist: true,
+		})
+		console.log(`New Role Created`)
+		} else {
+		console.log(`Existing Role`)
+		roleLieutenant.edit({
+			color: '#00ffff', //cyan
+			mentionable: true,
+			hoist: true,
+		})
+		}
+		//Sergeant
+		let roleSergeant = interaction.guild.roles.cache.find(role => role.name === "Sergeant");
+		if (!roleSergeant) {
+			console.log(`No Role Found`)
+			let roleSergeant = await interaction.guild.roles.create({ 
+				name: 'Sergeant',
+				color: '#00ff80', //spring green
+				mentionable: true,
+				hoist: true,
+		})
+		console.log(`New Role Created`)
+		} else {
+		console.log(`Existing Role`)
+		roleSergeant.edit({
+			color: '#00ff80', //spring green
+			mentionable: true,
+			hoist: true,
+		})
+		}
+		//Corporal
+		let roleCorporal = interaction.guild.roles.cache.find(role => role.name === "Corporal");
+		if (!roleCorporal) {
+			console.log(`No Role Found`)
+			let roleCorporal = await interaction.guild.roles.create({ 
+				name: 'Corporal',
+				color: '#00ff00', //green
+				mentionable: true,
+				hoist: true,
+		})
+		console.log(`New Role Created`)
+		} else {
+		console.log(`Existing Role`)
+		roleCorporal.edit({
+			color: '#00ff00', //green
+			mentionable: true,
+			hoist: true,
+		})
+		}
+		//Private
+		let rolePrivate = interaction.guild.roles.cache.find(role => role.name === "Private");
+		if (!rolePrivate) {
+			console.log(`No Role Found`)
+			let rolePrivate = await interaction.guild.roles.create({ 
+				name: 'Private',
+				color: '#2e8f37', //forest green
+				mentionable: true,
+				hoist: true,
+		})
+		console.log(`New Role Created`)
+		} else {
+		console.log(`Existing Role`)
+		rolePrivate.edit({
+			color: '#2e8f37', //forest green
+			mentionable: true,
+			hoist: true,
+		})
+		}
+		await interaction.editReply({ empheral: true, content: `Congratulations <@${interaction.user.id}>, Rank Roles Added!` })
 	},
 };

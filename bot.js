7eec8140afcd8d42275d340925fa0c7b9b34f827
John O'Brien <JohnoUK79@gmail.com>
2022-11-06 11:30:25 +0000

    Added Partial & Mentions

--- bot.js
+++ bot.js
@@ -41,7 +41,12 @@ const client = new Client({
         Intents.FLAGS.DIRECT_MESSAGE_TYPING,
         Intents.FLAGS.GUILD_SCHEDULED_EVENTS,
     ],
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER'],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER', 'GUILD_MEMBER', 'GUILD_SCHEDULED_EVENT'],
+    
+    allowedMentions: {
+        parse: ["roles", "users", "everyone"],
+        repliedUser: true
+    },
     
 })
 

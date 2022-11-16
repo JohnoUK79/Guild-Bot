eb952a3757e9144170adf0b4061c3e5bf9514cfb
John O'Brien <JohnoUK79@gmail.com>
2022-11-16 01:53:26 +0000

    Added Embed to Queue

--- commands/queue.js
+++ commands/queue.js
@@ -5,6 +5,8 @@ module.exports = {
 		.setName('queue')
 		.setDescription('Shows the first 10 songs in the Queue!'),
 	async execute(interaction) {
+		guildIcon = interaction.member.guild.iconURL();
+		guildName = interaction.member.guild.name
 		const { botJukebox } = require('../bot')
 		const player = botJukebox
 		const queue = player.getQueue(interaction.guild);
@@ -14,8 +16,14 @@ module.exports = {
 				ephemeral: true,
 			})
 		}
+		// if (queue.length === 0) {
+		// 	return interaction.reply({
+		// 		content: `There is No More **Songs** playing!`,
+		// 		ephemeral: true,
+		// 	})
+		// }
 		const queueString = queue.tracks.slice(0, 10).map((song, i) => {
-			return `${i + 1}) [${song.duration}]\` ${song.title} - <@${song.requestedBy.id}>`;
+			return `\n(${i + 1}) [${song.duration}] - ${song.title} - ${song.requestedBy}`;
 		}).join('\n');
 
 		const currentSong = queue.current;
@@ -23,8 +31,19 @@ module.exports = {
 		return interaction.reply({
 			embeds: [
 				new EmbedBuilder()
-					.setDescription(`**Currently Playing**\n\` ${currentSong.title} - <@${currentSong.requestedBy.id}>\n\n**Queue:**${queueString}`)
+					.setColor('#ffff00')
+					.setDescription(`**Currently Playing**\n${currentSong.title}`)
 					.setThumbnail(currentSong.thumbnail)
+					.addFields(
+						{ name: `Requested By:`, value: `<@${currentSong.requestedBy.id}>`, inline: true },
+						{ name: 'Playing Next.', value: `${queueString}`, inline: false },
+						)
+					.setImage(currentSong.thumbnail)
+					.setTimestamp()
+					.setFooter({ text: `${guildName} - Jukebox.`, iconURL: `${guildIcon}` })
+					.setAuthor({ name: interaction.member.displayName, iconURL: interaction.member.displayAvatarURL({ dynamic: true })})
+
+
 			]
 		})
 

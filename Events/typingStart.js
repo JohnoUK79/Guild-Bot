3f769b445e043beca62fd82d6b0aa4a74c15c7b0
John O'Brien <JohnoUK79@gmail.com>
2022-12-04 22:01:07 +0000

    Timestamp Typing

--- Events/typingStart.js
+++ Events/typingStart.js
@@ -1,7 +1,9 @@
+time = require('../config/timestamp')
+setDate = time.UTCdefault()
 
 module.exports = {
     name: 'typingStart',
     async execute(typing) {
-        console.log(`Typing: ${typing.user.username}#${typing.user.discriminator} is typing...`)
+        console.log(`Typing ${setDate}: ${typing.user.username}#${typing.user.discriminator} is typing...`)
     }
 };

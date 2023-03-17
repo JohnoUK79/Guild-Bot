c1178601136259677d8be4cc046c6506b792c4ee
JohnoUK79 <johnouk79@gmail.com>
2023-03-17 21:33:49 +0000

    Added Gift

---
+++ commands/gift.js
@@ -0,0 +1,57 @@
+const { PermissionFlagsBits, SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const sql = require("../config/Database");
+const timestamp = require('../config/timestamp');
+setDate = timestamp.UTCdefault()
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator | PermissionFlagsBits.ManageGuild)
+        .setName("gift")
+        .setDescription("Gift War-Coins!")
+		.addUserOption(option =>
+			option
+				.setName('player')
+				.setDescription('Member to Gift Coins to!')
+				.setRequired(true)
+			)
+		.addIntegerOption(option =>
+			option
+				.setName('amount')
+				.setDescription('Amount of War-Coins to gift!')
+				.setRequired(true)
+			),
+
+    async execute(interaction) {
+		const owner = '322100798651760640'
+		await interaction.deferReply({
+			fetchReply: true,
+			ephemeral: true,
+		})
+		const player = interaction.options.getUser('player');
+		const amount = interaction.options.getInteger('amount');
+
+		guildIcon = interaction.member.guild.iconURL();
+		guildName = interaction.member.guild.name
+		Economy = await sql.Execute(`SELECT * FROM levels WHERE discord_id = ${player.id}`)
+		const embed = new EmbedBuilder();
+			embed
+				.setColor('#ff5b05')
+				.setThumbnail(guildIcon)
+				.setTimestamp()
+				.setAuthor({ name: interaction.member.displayName, iconURL: interaction.member.displayAvatarURL({ dynamic: true })})
+				.setFooter({ text: `${guildName} - Daily Rewards`, iconURL: `${guildIcon}`});
+		if (player.id != owner) {return interaction.editReply(`Only the Owner of the Bot can gift coins!`)}
+       	const wallet = Economy[0].war_coins
+		const bank = Economy[0].war_chest
+		const newWallet = wallet + amount;
+		const dailyUpdate = await sql.Execute(`UPDATE levels SET war_coins = '${newWallet}' WHERE discord_id = ${interaction.member.id}`)
+		console.log(`Daily: ${interaction.member.displayName} ${dailyUpdate.info}`)
+		embed
+			.setDescription(`${interaction.member} You sucessfully gifted **$${amount.toLocaleString()} War-Coins** to ${player}`)
+			.addFields(
+				{ name: `War-Coins:`, value: `$${newWallet.toLocaleString()}`, inline: true }, 
+				{ name: `War-Chest:`, value: `$${bank.toLocaleString()}`, inline: true },
+			)
+		return interaction.editReply({ embeds: [embed] });
+			} 
+		}

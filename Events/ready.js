8f324246b2c88215a0d64b73af49bea35a4422b6
JohnoUK79 <johnouk79@gmail.com>
2023-04-14 00:20:19 +0100

    Added Cron Job for Presences

--- Events/ready.js
+++ Events/ready.js
@@ -21,6 +21,14 @@ module.exports = {
 
         console.log(`================ Warpath BOT Ready! ================`);
         //Presence Update
+        client.guilds.cache.forEach(async (guild) => {
+            guild.members.fetch({ withPresences: true }).then(fetchedMembers => {
+                const totalOnline = fetchedMembers.filter(member => member.presence?.status === 'online');
+                // Now you have a collection with all online member objects in the totalOnline variable
+                console.log(`There are currently ${totalOnline.size} members online in this guild ${guild.name}!`);
+                client.user.setPresence({ activities: [{ name: `Warpath Battle-Bot with ${totalOnline.size.toLocaleString()} Online Members!` }], status: 'Online' });
+            });
+        })    
         const memberRefresh = nodeCron.schedule("0,15,30,45 * * * *", () => {
         client.guilds.cache.forEach(async (guild) => {
         guild.members.fetch({ withPresences: true }).then(fetchedMembers => {

db3a8eb803cb46550677f14732774a43bec80f7d
JohnoUK79 <johnouk79@gmail.com>
2023-05-21 09:34:29 +0100

    Added ChatGPT to Mentions

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -19,10 +19,34 @@ module.exports = {
 		dmReceived(message)
 		} catch (err) {console.log(err)}
 		}
+
 		//Chat Bot Message
 		if (message.content.startsWith(`Battle-Bot`)) {
-			//message.content.slice(4)
 			const target = message.content.slice(11)
+			const configuration = new Configuration({
+				apiKey: OPENAI_API_KEY,
+			  });
+			  const openai = new OpenAIApi(configuration);
+			async function runCompletion () {
+			const reponse = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: target,
+			max_tokens:4000
+			});
+			console.log(`Battle-Bot Prefix:${target}`)
+			const reply = reponse.data.choices[0].text
+			if (!reply) return
+			message.reply({
+				content: `${reply}`
+			})}
+			runCompletion();
+
+			console.log(`Battle-Bot AI Text Chat`)
+		}
+		const { client } = require('../bot')
+		if (message.mentions.users.has(client.user.id)){
+			const target = message.content.slice(23)
+			console.log(`Mention:${target}`)
 			const configuration = new Configuration({
 				apiKey: OPENAI_API_KEY,
 			  });
@@ -40,10 +64,9 @@ module.exports = {
 				content: `${reply}`
 			})}
 			runCompletion();
-	
-			console.log(`Battle-Bot Chat`)
-		}
 
+			console.log(`Battle-Bot AI Mentioned Chat`)
+		}
 		//Translate Prefix
 		if (message.content.startsWith(`!t`)) {
 		try {

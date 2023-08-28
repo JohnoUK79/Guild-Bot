6cb82f8dfde7e255d116af00d85b6311e60569d3
JohnoUK79 <johnouk79@gmail.com>
2023-08-28 16:41:43 +0100

    Updated Register

--- commands/register.js
+++ commands/register.js
@@ -10,17 +10,27 @@ module.exports = {
         .setDescription("Register for Battle-Bot!"),
 
     async execute(interaction) {
-		const embed = new EmbedBuilder();
-			embed
-				.setColor('#ff5b05')
-				.setThumbnail(guildIcon)
-                .setDescription(`Welcome **${interaction.member.displayName}** you are now registered for **Battle-Bot**\nPlease use **/Battle-Bot Profile** to get started.\nMention ${interaction.member.client.user} for Help!`)
-				.setTimestamp()
-				.setAuthor({ name: interaction.member.displayName, iconURL: interaction.member.displayAvatarURL({ dynamic: true })})
-				.setFooter({ text: `${guildName} - Welcome to Battle-Bot`, iconURL: `${guildIcon}`});
-                interaction.reply({
-                    embeds: [embed]
-                })
+        const Levels = await sql.Execute(`SELECT * FROM levels WHERE discord_id = '${interaction.member.id}'`)
+        const embed = new EmbedBuilder();
+        embed
+            .setColor('#ff5b05')
+            .setThumbnail(guildIcon)
+            .setDescription(`Welcome **${interaction.member.displayName}** you are now registered for **Battle-Bot**\nPlease use **/Battle-Bot Profile** to get started.\nMention ${interaction.member.client.user} for Help!`)
+            .setTimestamp()
+            .setAuthor({ name: interaction.member.displayName, iconURL: interaction.member.displayAvatarURL({ dynamic: true })})
+            .setFooter({ text: `${guildName} - Welcome to Battle-Bot`, iconURL: `${guildIcon}`});
+
+        if (!Levels) {
+            interaction.reply({
+                embeds: [embed]
+            })
+        } else {
+            embed   
+                .setDescription(`**${interaction.member.displayName}** you are already registered for **Battle-Bot**\nPlease use **/Battle-Bot Profile** to get started.\nMention ${interaction.member.client.user} for Help!`)
+            interaction.reply({
+                embeds: [embed]
+            })
+        }
     }
 
 };
